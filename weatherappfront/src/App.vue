68c8f86221cd66c52b35ec2e089b8c8b43290974
Miczu <miczu0607@gmail.com>
2022-06-01 12:26:18 +0200

    initialize commit, added vuetify to project and create dialog for login user

---
+++ weatherappfront/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <login-dialog />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import LoginDialog from "@/components/loginDialog.vue";
+
+export default Vue.extend({
+  name: "App",
+  components: { LoginDialog },
+  data: () => ({
+    //
+  }),
+});
+</script>
